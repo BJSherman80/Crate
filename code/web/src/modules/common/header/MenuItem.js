4c30a5886ed391309b8868606216cef0504330ff
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-02-03 11:54:39 -0700

    Install redux-dev-tools

--- code/web/src/modules/common/header/MenuItem.js
+++ code/web/src/modules/common/header/MenuItem.js
@@ -1,56 +1,62 @@
 // Imports
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, withRouter } from 'react-router-dom'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, withRouter } from "react-router-dom";
 
 // UI Imports
-import { white } from '../../../ui/common/colors'
-import { primary, secondary } from '../../../ui/common/gradients'
-import { level1 } from '../../../ui/common/shadows'
+import { white } from "../../../ui/common/colors";
+import { primary, secondary } from "../../../ui/common/gradients";
+import { level1 } from "../../../ui/common/shadows";
 
 // Component
 const MenuItem = (props) => {
-  const { children, to, type, active, style, section } = props
+  const { children, to, type, active, style, section } = props;
 
   const isActiveRoute = () => {
-    const currentSection = props.location.pathname.split('/')[1]
+    const currentSection = props.location.pathname.split("/")[1];
 
-    return (currentSection === to.split('/')[1] && currentSection === section)
-      || props.location.pathname === to
-      || active
-  }
-
+    return (
+      (currentSection === to.split("/")[1] && currentSection === section) ||
+      props.location.pathname === to ||
+      active
+    );
+  };
+  //NTS: Fix Header MenuItem styling
+  // display: flex; float: none; paddingLeft: 1em
   return (
     <Link
       to={to}
-      style={
-        Object.assign({
-          padding: '0.6em 1em',
-          textTransform: 'uppercase',
-          color: white
-        }, isActiveRoute() ? {
-          backgroundImage: (type === 'secondary' ? secondary : primary),
-          borderRadius: '1.4em',
-          boxShadow: level1
-        } : style)
-      }
+      style={Object.assign(
+        {
+          padding: "0.6em 1em",
+          textTransform: "uppercase",
+          color: white,
+        },
+        isActiveRoute()
+          ? {
+              backgroundImage: type === "secondary" ? secondary : primary,
+              borderRadius: "1.4em",
+              boxShadow: level1,
+            }
+          : style
+      )}
     >
       {children}
     </Link>
-  )
-}
+  );
+};
 
 // Component Properties
 MenuItem.propTypes = {
   to: PropTypes.string.isRequired,
   type: PropTypes.string,
   active: PropTypes.bool,
-  style: PropTypes.object
-}
+  style: PropTypes.object,
+};
 MenuItem.defaultProps = {
-  type: 'secondary',
+  type: "secondary",
   active: false,
-  style: {}
-}
+  style: {},
+};
 
-export default withRouter(MenuItem)
+export default withRouter(MenuItem);
