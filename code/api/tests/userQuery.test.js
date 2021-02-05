a05b0c7174fbf9b673830ba2be71cd5a8f237cfa
Eugene Theriault <hybridbassist@gmail.com>
2021-02-04 17:05:04 -0800

    Create failing expectation for status code
    Currently failing for the wrong reasons; GraphQL should theoretically be
    returning a 200 OK even if it doesn't find the requested user.

--- code/api/tests/userQuery.test.js
+++ code/api/tests/userQuery.test.js
@@ -1,20 +1,25 @@
-const request = require('supertest')
-const app = require('../src/index')
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+
 describe('GraphQL user query', () => {
   it('should return a single user', async () => {
+    const userId = 1
     const response = await request(app)
       .post('/')
       .send({
-        query: `query user(id: ${userId}) {
-                  id
-                  name
-                  role
-                  email
+        query: `query {
+                  user(id: ${userId}) {
+                    id
+                    name
+                    role
+                    email
+                  }
                 }`
       })
-      .expect(200)
-      .end(function(error, response){
-        if ( error ) throw error;
-      });
-    });
+
+      expect(response.status).toBe(200);
+  });
 });
+
