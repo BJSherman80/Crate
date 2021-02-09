510aad62862751b39c510dc3e96495aba244d8cd
Will Dunlap <dunlapww@gmail.com>
2021-02-08 18:34:10 -0700

    add new user fields to user model

--- code/api/src/modules/user/model.js
+++ code/api/src/modules/user/model.js
@@ -2,21 +2,19 @@
 
 // User
 module.exports = function(sequelize, DataTypes) {
-  let User = sequelize.define('users', {
-    name: {
-      type: DataTypes.STRING
-    },
-    email: {
-      type: DataTypes.TEXT
-    },
-    password: {
-      type: DataTypes.TEXT
-    },
-    role: {
-      type: DataTypes.TEXT
-    }
+  let User = sequelize.define('users', 
+  {
+    name: { type: DataTypes.STRING },
+    email: { type: DataTypes.TEXT },
+    password: { type: DataTypes.TEXT },
+    role: { type: DataTypes.TEXT },
+    profileImage: { type: DataTypes.STRING },
+    streetAddress: { type: DataTypes.STRING },
+    city: { type: DataTypes.STRING },
+    state: { type: DataTypes.STRING },
+    zip: { type: DataTypes.STRING },
+    description: { type: DataTypes.STRING }
   })
-
   User.associate = function(models) {
     User.hasMany(models.Subscription)
   }
