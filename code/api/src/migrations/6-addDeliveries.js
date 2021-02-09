227099337e23352f271cac7c6641d681b82d90e4
Will Dunlap <dunlapww@gmail.com>
2021-02-08 19:00:55 -0700

    create addDeliveries migration

---
+++ code/api/src/migrations/6-addDeliveries.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('deliveries', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      subscriptionId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'subscriptions',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      deliveryDate: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
