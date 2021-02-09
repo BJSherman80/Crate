523f899ddc37683fdf880a0da8685ed18e52f9d4
Will Dunlap <dunlapww@gmail.com>
2021-02-09 16:13:24 -0700

    add factories folder and file

--- code/api/src/modules/delivery/tests/deliveryQuery.test.js
+++ code/api/src/modules/delivery/tests/deliveryQuery.test.js
@@ -3,7 +3,7 @@ import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema from '../../../setup/schema/'
 
-describe('user queries', () => {
+describe('delivery queries', () => {
   let server = express();
   beforeAll(() => {
     server.use(
@@ -13,25 +13,25 @@ describe('user queries', () => {
         graphiql: false
       })
     )
-    
+
   })
 
   it('can successfully query all fields from deliveries table', async (done) => {
     const response = await request(server)
     .post('/')
-    .send({query: `{delivery(id: 1) {
-                      subscriptionId
-                      deliveryDate
-                      createdAt
-                      updatedAt
-                      }
-                    }`})
+    .send({query: `{
+      delivery(id:1){
+        deliveryDate
+        createdAt
+        updatedAt
+      }
+    }`})
     .expect(200)
+    
 
-    expect(response.body.data.user).toHaveProperty('subscriptionId')
-    expect(response.body.data.user).toHaveProperty('deliveryDate')
-    expect(response.body.data.user).toHaveProperty('createdAt')
-    expect(response.body.data.user).toHaveProperty('updatedAt')
+    expect(response.body.data.delivery).toHaveProperty('deliveryDate')
+    expect(response.body.data.delivery).toHaveProperty('createdAt')
+    expect(response.body.data.delivery).toHaveProperty('updatedAt')
     done();
   })
 })
--- code/api/src/modules/user/tests/userQuery.test.js
+++ code/api/src/modules/user/tests/userQuery.test.js
@@ -1,7 +1,8 @@
 import request from 'supertest';
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import schema from '../../../setup/schema/'
+import schema from '../../../setup/schema/';
+
 
 describe('user queries', () => {
   let server = express();
