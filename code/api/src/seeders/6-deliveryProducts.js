0057d09979710f6f5685cb958d46dfc65b745e7b
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-02-13 18:26:05 -0700

    Add date parse func to ModalForm; Set defaultValue to today

--- code/api/src/seeders/6-deliveryProducts.js
+++ code/api/src/seeders/6-deliveryProducts.js
@@ -1,22 +1,26 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('deliveryProducts', [
-      {
-        deliveryId: 1,
-        productId: 1,
-        wasReturned: false
-      },
-      {
-        productId: 2,
-        deliveryId: 2,
-        wasReturned: true
-      }
-    ], {})
+    return queryInterface.bulkInsert(
+      "deliveryProducts",
+      [
+        {
+          deliveryId: 3,
+          productId: 1,
+          wasReturned: false,
+        },
+        {
+          productId: 2,
+          deliveryId: 4,
+          wasReturned: true,
+        },
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('deliveryProducts', null, {});
-  }
-}
+    return queryInterface.bulkDelete("deliveryProducts", null, {});
+  },
+};
